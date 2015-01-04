32012e89c36eab1bf6115c9c7c043dcbbed61ed4
gtovar1985 <gtovar@incaztech.com>
2015-01-04 12:50:29 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Purpurapromocionales::Application.config.secret_key_base = 'ff27fca1752fb758244e1f9b62b02446ed398a2433daec4f07bb8568777eeaedbe3a332008d432494968a9f30c0ba0acbb5dbad67ca9389993bb47f2bfc2a391'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Purpurapromocionales::Application.config.session_store :cookie_store, key: '_purpurapromocionales_session'
