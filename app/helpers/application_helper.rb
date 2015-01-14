720281ee6aff33a73538f0f67e6ccd30d1f18fa8
gtovar1985 <gtovar@incaztech.com>
2015-01-13 22:49:53 -0600

    Devise is READYgit status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cp(path)                                          
+    "active" if current_page?(path)                                       
+  end                                                                           
+
+  def title(page_title)                        
+    content_for :title, page_title.to_s 
+  end     
 end
