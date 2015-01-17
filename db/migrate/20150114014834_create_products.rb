e1f1cc7ff09c56b32a3d82713b111985c08cbb14
gtovar1985 <gtovar@incaztech.com>
2015-01-17 15:03:22 -0600

    Change Datatype of Migration, formated View New product, add title for every page helper

--- db/migrate/20150114014834_create_products.rb
+++ db/migrate/20150114014834_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
-      t.string :description
+      t.text :description
       t.string :price
       t.string :image
       t.references :category, index: true
