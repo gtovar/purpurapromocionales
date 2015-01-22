e9ce3cc95cab394d76eec86527858a08fb60a6e6
gtovar1985 <gtovar@incaztech.com>
2015-01-21 20:17:01 -0600

    Add new field Short description

--- db/migrate/20150114014834_create_products.rb
+++ db/migrate/20150114014834_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.text :short
       t.text :description
       t.string :price
       t.string :image
