0787513cfb0a62c81eab42d6a1d1c0a3f5894254
gtovar1985 <gtovar@incaztech.com>
2015-01-21 20:20:37 -0600

    add new field

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150114020045) do
 
   create_table "products", force: true do |t|
     t.string   "name"
+    t.text     "short"
     t.text     "description"
     t.string   "price"
     t.string   "image"
